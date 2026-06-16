330dd6cbf235254eb69613a70a36801e95f9aaee
GodsonArockiaRaj007 <godsonarockiaaraj.26csa@licet.ac.in>
2026-06-16 09:50:38 +0530

    Day4 Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
     <div className="flex justify-between bg-purple-950 h-18  p-2 items-center">
         <div className=" w-14 h-14 rounded-4xl">
-            <img className="rounded-4xl" src="../../public/download.jpg" alt="" />
+            <img className="rounded-4xl" src="./download.jpg" alt="" />
         </div>
         <div >
               <h1 className="bg-white rounded-3xl text-blue-600 font-bold p-3 text-2xl ">Soft Logic</h1>
