f9b3c9fe0678d1c5036fecd3169f63c06b2c0f58
GodsonArockiaRaj007 <godsonarockiaaraj.26csa@licet.ac.in>
2026-06-18 23:08:16 +0530

    Day2 added

--- src/Day2/pages/Register.jsx
+++ src/Day2/pages/Register.jsx
@@ -9,7 +9,7 @@ const navigate=useNavigate()
   return (
     <>
     <div className="bg-red-300 h-screen  p-5">
-        <button onClick={handleClick} className=" bg-blue-700 flex gap-5 items-center p-3 rounded-3xl  font-bold  "><img src="../../public/back.png" className="w-10 h-10 rounded-4xl" alt="" /> Back</button>
+        <button onClick={handleClick} className=" bg-blue-700 flex gap-5 items-center p-3 rounded-3xl  font-bold  "><img src="./back.png" className="w-10 h-10 rounded-4xl" alt="" /> Back</button>
     <div className="  flex justify-center mt-30">
 
       <form className=" border-2 border-black w-fit rounded-3xl p-5 space-y-3  bg-white  ">
